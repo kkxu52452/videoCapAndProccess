81e0d0cb1abed74ed6962fad5859cd9b44caf201
kk Xu <kkxu52452@gmail.com>
2019-06-18 18:11:31 +0800

    BaiduFaceAPI

--- main.go
+++ main.go
@@ -1,81 +1,66 @@
-// What it does:
-//
-// This example shows, how to use pretrained SSD (Single Shot Detection) detection networks in gocv.
-// Here, we detect human faces from the camera, but the setup is similar for any other kind of object detection.
-//
-// Download the (small, 5.1mb) Caffe model file from:
-// https://raw.githubusercontent.com/opencv/opencv_3rdparty/dnn_samples_face_detector_20180205_fp16/res10_300x300_ssd_iter_140000_fp16.caffemodel
-//
-// Also, you will need the prototxt file:
-// https://raw.githubusercontent.com/opencv/opencv/master/samples/dnn/face_detector/deploy.prototxt
-//
-// How to run:
-//
-// 		go run ./cmd/ssd-facedetect/main.go 0 [protofile] [modelfile] [videofile]
-//
-// +build example
-
 package main
 
 import (
+	"encoding/base64"
+	"encoding/json"
 	"fmt"
 	"image"
 	"image/color"
+	"io/ioutil"
+	"net/http"
 	"os"
+	"strings"
 
 	"gocv.io/x/gocv"
 )
 
-func min(a, b float32) float32 {
-	if a < b {
-		return a
-	}
-	return b
+type Location struct {
+	Left 	int 	`json:"left"`
+	Top  	int 	`json:"top"`
+	width 	int 	`json:"left"`
+	height 	int 	`json:"height"`
+	rotation float64 	`json:"rotation"`
 }
 
-func max(a, b float32) float32 {
-	if a > b {
-		return a
-	}
-	return b
+type Detail struct {
+	location 		Location   	`json:"location"`
+	Probability 	float64 	`json:"face_probability"`
 }
+
+type Result struct {
+	face_list []Detail  `json:"face_list"`
+}
+
+type Response struct {
+	ReturnMsg string 	`json:"error_msg"`
+	DetecResult Result  `json:"result"`
+}
+
 
 func main() {
-	if len(os.Args) < 4 {
-		fmt.Println("How to run:\nssd-facedetect [camera ID] [protofile] [modelfile] [videofile]")
-		return
-	}
 
 	// parse args
 	deviceID := os.Args[1]
-	proto := os.Args[2]
-	model := os.Args[3]
-	saveFile := os.Args[4]
+	saveFile := os.Args[2]
 
-	// open capture device
+	// open webcam
 	webcam, err := gocv.OpenVideoCapture(deviceID)
 	if err != nil {
-		fmt.Printf("Error opening video capture device: %v\n", deviceID)
+		fmt.Printf("error opening video capture device: %v\n", deviceID)
 		return
 	}
 	defer webcam.Close()
 
-	window := gocv.NewWindow("SSD Face Detection")
+	// open display window
+	window := gocv.NewWindow("Face Detect")
 	defer window.Close()
 
+	// prepare image matrix
 	img := gocv.NewMat()
 	defer img.Close()
 
-	// open DNN classifier
-	net := gocv.ReadNetFromCaffe(proto, model)
-	if net.Empty() {
-		fmt.Printf("Error reading network model from : %v %v\n", proto, model)
-		return
-	}
-	defer net.Close()
-
-	green := color.RGBA{0, 255, 0, 0}
-	fmt.Printf("Start reading device: %v\n", deviceID)
+	// color for the rect when faces detected
+	blue := color.RGBA{0, 0, 255, 0}
 
 	writer, err := gocv.VideoWriterFile(saveFile, "MJPG", 25, img.Cols(), img.Rows(), true)
 	if err != nil {
@@ -84,6 +69,7 @@ func main() {
 	}
 	defer writer.Close()
 
+	fmt.Printf("Start reading device: %v\n", deviceID)
 	for i := 0; i < 100; i++ {
 		if ok := webcam.Read(&img); !ok {
 			fmt.Printf("Device closed: %v\n", deviceID)
@@ -93,53 +79,51 @@ func main() {
 			continue
 		}
 
-		W := float32(img.Cols())
-		H := float32(img.Rows())
+		// detect faces
+		resp := callFaceDetecAPI(img)
+		fmt.Printf("Face Detect: %s", resp.ReturnMsg)
 
-		// convert image Mat to 96x128 blob that the detector can analyze
-		blob := gocv.BlobFromImage(img, 1.0, image.Pt(128, 96), gocv.NewScalar(104.0, 177.0, 123.0, 0), false, false)
-		defer blob.Close()
-
-		// feed the blob into the classifier
-		net.SetInput(blob, "data")
-
-		// run a forward pass through the network
-		detBlob := net.Forward("detection_out")
-		defer detBlob.Close()
-
-		// extract the detections.
-		// for each object detected, there will be 7 float features:
-		// objid, classid, confidence, left, top, right, bottom.
-		detections := gocv.GetBlobChannel(detBlob, 0, 0)
-		defer detections.Close()
-
-		for r := 0; r < detections.Rows(); r++ {
-			// you would want the classid for general object detection,
-			// but we do not need it here.
-			// classid := detections.GetFloatAt(r, 1)
-
-			confidence := detections.GetFloatAt(r, 2)
-			if confidence < 0.5 {
-				continue
-			}
-
-			left := detections.GetFloatAt(r, 3) * W
-			top := detections.GetFloatAt(r, 4) * H
-			right := detections.GetFloatAt(r, 5) * W
-			bottom := detections.GetFloatAt(r, 6) * H
-
-			// scale to video size:
-			left = min(max(0, left), W-1)
-			right = min(max(0, right), W-1)
-			bottom = min(max(0, bottom), H-1)
-			top = min(max(0, top), H-1)
-
-			// draw it
-			rect := image.Rect(int(left), int(top), int(right), int(bottom))
-			gocv.Rectangle(&img, rect, green, 3)
+		// draw a rectangle around each face on the original image,
+		// along with
+		details := resp.DetecResult.face_list
+		for _, d := range details {
+			loc := d.location
+			rotrect := gocv.RotatedRect{nil,nil,image.Point{loc.Left,loc.Top},loc.width,loc.height,loc.rotation}
+			gocv.Rectangle(&img, rotrect.BoundingRect, blue, 3)
+			//size := gocv.GetTextSize("Human", gocv.FontHersheyPlain, 1.2, 2)
+			//pt := image.Pt(r.Min.X+(r.Min.X/2)-(size.X/2), r.Min.Y-2)
+			//gocv.PutText(&img, "Human", pt, gocv.FontHersheyPlain, 1.2, blue, 2)
 		}
 
 		writer.Write(img)
 	}
 }
 
+func callFaceDetecAPI(img gocv.Mat) Response {
+
+	url := "https://aip.baidubce.com/rest/2.0/face/v3/detect?access_token=24.455a0daccbd329c48d63307cfc3ac5f8.2592000.1563431485.282335-16550271"
+
+	payload := base64.StdEncoding.EncodeToString(img.ToBytes())
+
+	req, _ := http.NewRequest("POST", url, strings.NewReader(payload))
+
+	req.Header.Add("Content-Type", "application/x-www-form-urlencoded")
+
+	res, _ := http.DefaultClient.Do(req)
+
+	defer res.Body.Close()
+
+	// read json http response
+	jsonDataFromHttp, err := ioutil.ReadAll(res.Body)
+	if err != nil {
+		panic(err)
+	}
+
+	var resp Response
+	err = json.Unmarshal(jsonDataFromHttp, resp)
+	if err != nil {
+		panic(err)
+	}
+
+	return resp
+}
